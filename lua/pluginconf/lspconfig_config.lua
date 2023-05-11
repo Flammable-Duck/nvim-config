6a483c5d508e77726b73f62bb980879dc0246bfb
Flammable Duck <flammableduck@protonmail.com>
2023-05-11 14:23:33 -0700

    replaced cmp_nvim_lsp.update_capabilities() with newer function

--- lua/pluginconf/lspconfig_config.lua
+++ lua/pluginconf/lspconfig_config.lua
@@ -94,7 +94,7 @@ vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {f
 
 -- Add additional capabilities supported by nvim-cmp
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = { 'rust_analyzer', 'pyright', 'tsserver', 'solargraph', 'gopls'}
