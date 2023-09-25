1cd1966d15feb7189c2ac8375f617bd0ec6a1e3e
Flammable Duck <flammableduck@protonmail.com>
2023-09-25 01:02:20 -0700

    treesitter: disabled ensure_enstalled

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,8 +6,8 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "cpp", "go", "lua", "ruby", "vim",
-              "vimdoc", "query", "elixir", "javascript", "html" },
+          -- ensure_installed = { "c", "cpp", "go", "lua", "ruby", "vim",
+          --     "vimdoc", "query", "elixir", "javascript", "html" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
