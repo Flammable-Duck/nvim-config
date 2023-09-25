cfe9d7e35ebdad4903d28a0351740ca39775c55b
Flammable Duck <flammableduck@protonmail.com>
2023-09-25 00:55:10 -0700

    added treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+    {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "cpp", "go", "lua", "ruby", "vim",
+              "vimdoc", "query", "elixir", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }
+}
